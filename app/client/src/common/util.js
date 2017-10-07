2088115aeb95eec98d5a700068419deb1edec0c6
aquilino <aquilinopintoa@gmail.com>
2017-10-07 13:06:45 -0400

    validation

--- app/client/src/common/util.js
+++ app/client/src/common/util.js
@@ -3,39 +3,45 @@ import Validator from 'validator'
 import _ from 'lodash'
 
 function notNull(value, ){
-    return Validator.isEmpty(value)
+    return Validator.isEmpty(value) ? 'Is Required' : null 
 }
 
 function email(value){
-    return Validator.isEmail(value)
+    return Validator.isEmail(value) ? 'Invalid value' : null
 }
 
 function tektonlab(value){
-    return value.toLowerCase().indexOf('@tektonlab') > 0
+    return value.toLowerCase().indexOf('@tektonlab') < 0 ?
+        'only tektonlab domain'
+        :
+        null
 }
 
 export default function ValidateInput(applyVals, value, fieldName) {
     
     const errors = {}
 
-    applyVals.array.forEach(function(val) {
+    applyVals.array.every(function(val) {
+        const result
         switch (val) {
             case 'NOTNULL':
-                notNull(value, fieldName) 
+                result = notNull(value) 
                 break 
             case 'EMAIL':
-                email()
+                result = email()
                 break
             case 'TEKTONLAB': 
-                tektonlab()
+                result = tektonlab()
                 break
             default:
                 console.log("validation not available")
         }  
+        if(result)
+            errors[fieldName] = result
+
+        return !result
     });
 
-    return {
-        errors
-    }
+    return errors
 
 }
