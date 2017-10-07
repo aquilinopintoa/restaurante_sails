fae0230732eba27ea181a7fe3c792b51a6cd0a5a
aquilino <aquilinopintoa@gmail.com>
2017-10-07 11:16:38 -0400

    admin

---
+++ app/client/src/components/CashFlow/index.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import _ from 'lodash'
+import { build, actions } from '../../container_helpers'
+import FlatButton from 'material-ui/FlatButton'
+import {
+  Card, 
+  CardActions, 
+  CardHeader, 
+  CardMedia, 
+  CardTitle, 
+  CardText
+} from 'material-ui/Card'
+import {List, ListItem} from 'material-ui/List'
+
+class CashFlow extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      loading: false
+    }
+    this.handlerRefresh = this.handlerRefresh.bind(this)
+  }
+
+  async componentWillMount() {
+    await Promise.all([
+      this.props.getAllOrders()])
+  }
+
+  async handlerRefresh() {
+    await this.props.getAllOrders()
+  }
+
+  render() {
+    const styles = {
+      CashFlowContent: {
+        width:"300px"
+      },
+      textContent:{
+        textAlign: "right",
+        padding: "0px 30px"
+      }
+    }
+    
+    let sumTotal = 0
+    let sumCash = 0
+    let sumTDC = 0
+    this.props.orders.forEach(function(order){
+      order.type_payment === "EFECTIVO" ?
+        sumCash += order.total
+        :
+        sumTDC += order.total
+      sumTotal += order.total
+    })
+    return (
+      <Card style={styles.CashFlowContent}>
+          <CardHeader
+              title="Cash Flow"
+              subtitle={"last update: 10/11/2017 00:00:00"}
+          />
+          <CardTitle 
+              style={styles.textContent}
+              title={'Total '+sumTotal+' $'} />
+          <div style={styles.textContent}>
+            Cash &nbsp; {sumCash}
+          </div>
+          <div style={styles.textContent}>
+            Tdc &nbsp; {sumTDC}
+          </div>   
+      </Card>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  getAllOrders: actions.orders.getAll
+}
+
+const mapStateToProps = state => {
+  return {
+    orders: state.orders
+  }
+}
+
+export default build({
+  component: CashFlow,
+  mapDispatchToProps,
+  mapStateToProps,
+})
